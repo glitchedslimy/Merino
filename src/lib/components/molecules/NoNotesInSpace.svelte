c5f4c7698566aa2ca1ce1822b8097d3069a3d16a
glitchedslimy <jesusvelezmar@gmail.com>
2025-09-05 21:10:00 +0200

    fix: fixed logos in app

--- src/lib/components/molecules/NoNotesInSpace.svelte
+++ src/lib/components/molecules/NoNotesInSpace.svelte
@@ -1,5 +1,11 @@
 <script>
     import Button from "@components/atoms/Button.svelte";
+<<<<<<< Updated upstream:src/lib/components/molecules/NoNotesInSpace.svelte
+=======
+    import { createNote } from "../../lib/actions/workspace/notes";
+    import { activeSpace } from "../../lib/stores/workspace/spaces-store";
+    import woolyLogo from "../../assets/icons/WoolyStrokeClear.svg"
+>>>>>>> Stashed changes:src/components/organisms/NoNotesInSpace.svelte
 </script>
 
 <div
@@ -7,7 +13,11 @@
 >
     <div class="-mt-24 flex flex-col items-center justify-center space-y-md">
         <img
+<<<<<<< Updated upstream:src/lib/components/molecules/NoNotesInSpace.svelte
             src="./src/assets/icons/WoolyStroke.svg"
+=======
+            src={woolyLogo}
+>>>>>>> Stashed changes:src/components/organisms/NoNotesInSpace.svelte
             alt="Wooly Stroke Logo"
             width="250"
         />
